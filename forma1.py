f8fdf86441c9c6b291185ae91ef17b0e7e0e1ddd
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2026-02-25 09:19:47 +0100

    Végső verzió

--- forma1.py
+++ forma1.py
@@ -14,9 +14,70 @@ A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.tx
 """
 
 
-print("1. A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("2. A legtöbb futamot nyert versenyző: ____")
-print("3. A legkevesebb futamot nyert versenyző: ____")
-print("4. A legtöbb futamot teljesített versenyző: ____")
-print("5. Az átlagos futamszám: ____")
-print("***A legtöbb futamgyőzelmet szerző csapat: ____")
+# File beolvasás ---------------------------------------------------------------
+forma1 = []
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        f1 = {'Név': adatok[0], 'Csapat': adatok[1], 'Győzelmek száma': adatok[2],'Teljesített futamok száma': adatok[3]}
+        forma1.append(adatok)
+print(forma1)
+
+# 1.Feladat -------------------------------------------------------------------
+
+for f1 in forma1:
+    print(f1[0])
+print(f"Összesen {len(forma1)} versenyző szerepel.")
+
+
+# 2.Feladat -------------------------------------------------------------------
+
+legtobb_nyert_futam_szama = 0
+legtobb_nyert_futam = forma1
+for f1 in forma1:
+    if int(f1[2]) > legtobb_nyert_futam_szama:
+        legtobb_nyert_futam_szama = int(f1[2])
+        legtobb_nyert_futam = f1[0]
+
+
+print(f"Legtöbb futamot nyert versenyző: {legtobb_nyert_futam}")
+
+# 3.Feladat -------------------------------------------------------------------
+
+legkevesebb_nyert_futam_szama = int(forma1[0][2])
+legkevesebb_nyert_futam = forma1[0][0]
+for f1 in forma1:
+    if int(f1[2]) < legkevesebb_nyert_futam_szama:
+        legkevesebb_nyert_futam_szama = int(f1[2])
+        legkevesebb_nyert_futam = f1[0]
+
+print(f"Legkevesebb futamot nyert versenyző: {legkevesebb_nyert_futam}")
+
+# 4.Feladat -------------------------------------------------------------------
+
+legtobb_teljesitett_futam_szama = 0
+legtobb_teljesített_futam = forma1
+for f1 in forma1:
+    if int(f1[3]) > legtobb_teljesitett_futam_szama:
+        legtobb_teljesitett_futam_szama = int(f1[3])
+        legtobb_teljesített_futam = f1[0]
+
+print(f"Legtöbb futamot teljesített versenyző: {legtobb_teljesített_futam}")
+
+# 5.Feladat -------------------------------------------------------------------
+
+atlagos_futamszam = 0
+for f1 in forma1:
+    atlagos_futamszam += int(f1[3])
+atlagos_futamszam /= len(forma1)
+
+print(f"Átlagos futamszám: {atlagos_futamszam}")
+
+# Végső statisztika:
+with open('kiirt_adatok/statisztika.txt', 'w', encoding='utf8') as c:
+    c.write(f"1. A beolvasott fájlban összesen {len(forma1)} versenyző szerepel.\n")
+    c.write(f"2. A legtöbb futamot nyert versenyző: {legtobb_nyert_futam}\n")
+    c.write(f"3. A legkevesebb futamot nyert versenyző: {legkevesebb_nyert_futam}\n")
+    c.write(f"4. A legtöbb futamot teljesített versenyző: {legtobb_teljesített_futam}\n")
+    c.write(f"5. Az átlagos futamszám: {atlagos_futamszam}")
